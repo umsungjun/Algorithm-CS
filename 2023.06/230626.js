0a7d2846a2d9f9dfb47060e303770bf430275cb2
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-26 22:44:50 +0900

    홀짝 구분하기

--- 2023.06/230626.js
+++ 2023.06/230626.js
@@ -19,3 +19,30 @@ function solution(arr) {
   });
   return answer;
 }
+
+// 홀짝 구분하기
+// 문제 설명
+// 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+
+// 제한사항
+// 1 ≤ n ≤ 1,000
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  n = Number(input[0]);
+  // console.log(Number(input[0]))
+  if (Number(input[0]) % 2 === 0) {
+    console.log(`${Number(input[0])} is even`);
+  } else {
+    console.log(`${Number(input[0])} is odd`);
+  }
+});
