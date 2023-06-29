4c5bc44059915d025314ad522193b956a9f6980f
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-06-29 23:13:03 +0900

    문자열 반복해서 출력하기

---
+++ 2023.06/230629.js
@@ -0,0 +1,27 @@
+// 문제 설명
+// 문자열 str과 정수 n이 주어집니다.
+// str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+
+// 제한사항
+// 1 ≤ str의 길이 ≤ 10
+// 1 ≤ n ≤ 5
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let answer = "";
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  str = input[0];
+  n = Number(input[1]);
+
+  for (let i = 0; i < n; i++) {
+    answer += str;
+  }
+  console.log(answer);
+});
