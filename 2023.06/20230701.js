0535cd967be17291abda370f33a45141fb70d6a9
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-07-01 23:02:05 +0900

    특수문자 출력하기

--- 2023.06/20230701.js
+++ 2023.06/20230701.js
@@ -20,3 +20,21 @@ function solution(a, b) {
 
   return answer1 > answer2 ? answer1 : answer2;
 }
+
+// 문제 설명
+// 다음과 같이 출력하도록 코드를 작성해 주세요.
+
+// 출력 예시
+
+// !@#$%^&*(\'"<>?:;
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("close", function () {
+  console.log("!@#$%^&*(\\'\"<>?:;");
+  // console.log('\') SyntaxError
+});
