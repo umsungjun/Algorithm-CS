836993d12d65835b5608f9b7302c75518ef91e71
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-07-04 22:04:15 +0900

    기존 state방식

--- react_practice/src/App.tsx
+++ react_practice/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [id, setId] = useState("");
+  const [passWord, setPassWord] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      아이디 : <input type="text" onChange={(e) => setId(e.target.value)} />
+      <br />
+      비밀번호 :
+      <input type="password" onChange={(e) => setPassWord(e.target.value)} />
+      <br />
+      <button onClick={() => console.log(id, passWord)}>로그인</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
