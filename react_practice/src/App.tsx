ae1ce789b647acc0baa8ad18979ac9c8e9258256
umsungjun <umseongjun@naver.com>
2023-07-05 09:16:03 +0900

    useReducer 실습

--- react_practice/src/App.tsx
+++ react_practice/src/App.tsx
@@ -1,16 +1,56 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+type UserInfoState = {
+  id: string;
+  passWord: string;
+};
+
+type UserInfoAction =
+  | {
+      type: "setId";
+      payload: string;
+    }
+  | {
+      type: "setPassWord";
+      payload: string;
+    };
+
+function reducer(state: UserInfoState, action: UserInfoAction) {
+  switch (action.type) {
+    case "setId":
+      return {
+        ...state,
+        id: action.payload,
+      };
+    case "setPassWord":
+      return {
+        ...state,
+        passWord: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [id, setId] = useState("");
-  const [passWord, setPassWord] = useState("");
+  const [userInfo, dispatch] = useReducer(reducer, { id: "", passWord: "" });
   return (
     <>
-      아이디 : <input type="text" onChange={(e) => setId(e.target.value)} />
+      아이디 :{" "}
+      <input
+        type="text"
+        onChange={(e) => dispatch({ type: "setId", payload: e.target.value })}
+      />
       <br />
       비밀번호 :
-      <input type="password" onChange={(e) => setPassWord(e.target.value)} />
+      <input
+        type="password"
+        onChange={(e) =>
+          dispatch({ type: "setPassWord", payload: e.target.value })
+        }
+      />
       <br />
-      <button onClick={() => console.log(id, passWord)}>로그인</button>
+      <button onClick={() => console.log(userInfo)}>로그인</button>
     </>
   );
 }
