67c2172ad501102394e78e587c5838dc8fb5a516
umsungjun <umseongjun@naver.com>
2023-07-06 09:21:36 +0900

    바둑판 구현

--- react_practice/src/App.tsx
+++ react_practice/src/App.tsx
@@ -1,14 +1,36 @@
-import axios from "axios";
-import { useEffect } from "react";
+import { styled } from "styled-components";
 
 function App() {
-  useEffect(() => {
-    async () => {
-      const test = await axios.get("/cmn/auth/user");
-      console.log(test);
-    };
-  }, []);
-  return <></>;
+  return (
+    <GridBody>
+      <div>·</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div>·</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div>·</div>
+    </GridBody>
+  );
 }
 
 export default App;
+
+const GridBody = styled.div`
+  width: 50%;
+  height: 70vh;
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+
+  > div {
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 10rem;
+  }
+`;
