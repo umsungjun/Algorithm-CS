ace3101514625fb9d8baf6d6b6311e6b035015b5
umsungjun <umseongjun@naver.com>
2023-07-06 09:06:27 +0900

    axios

--- react_practice/src/App.tsx
+++ react_practice/src/App.tsx
@@ -1,58 +1,14 @@
-import { useReducer } from "react";
-
-type UserInfoState = {
-  id: string;
-  passWord: string;
-};
-
-type UserInfoAction =
-  | {
-      type: "setId";
-      payload: string;
-    }
-  | {
-      type: "setPassWord";
-      payload: string;
-    };
-
-function reducer(state: UserInfoState, action: UserInfoAction) {
-  switch (action.type) {
-    case "setId":
-      return {
-        ...state,
-        id: action.payload,
-      };
-    case "setPassWord":
-      return {
-        ...state,
-        passWord: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
-  const [userInfo, dispatch] = useReducer(reducer, { id: "", passWord: "" });
-  return (
-    <>
-      아이디 :{" "}
-      <input
-        type="text"
-        onChange={(e) => dispatch({ type: "setId", payload: e.target.value })}
-      />
-      <br />
-      비밀번호 :
-      <input
-        type="password"
-        onChange={(e) =>
-          dispatch({ type: "setPassWord", payload: e.target.value })
-        }
-      />
-      <br />
-      <button onClick={() => console.log(userInfo)}>로그인</button>
-    </>
-  );
+  useEffect(() => {
+    async () => {
+      const test = await axios.get("/cmn/auth/user");
+      console.log(test);
+    };
+  }, []);
+  return <></>;
 }
 
 export default App;
