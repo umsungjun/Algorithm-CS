5d2c2d343ef3921ffafd752e945ad5eec42ea30c
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-07-12 22:05:21 +0900

    DatePicker 적용 복습

--- react_practice/src/main.tsx
+++ react_practice/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import HashPwd from "./HashPwd.tsx";
 
+import DatePickerTest from "./DatePickerTest.tsx";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <HashPwd />
+  <DatePickerTest />
 );
