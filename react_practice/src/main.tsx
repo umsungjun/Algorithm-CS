096379fea79a7343d870b73041f756fe7a3cdd71
umsungjun <umseongjun@naver.com>
2023-07-24 17:14:19 +0900

    event버블링 연습

--- react_practice/src/main.tsx
+++ react_practice/src/main.tsx
@@ -6,7 +6,8 @@ import HashPwd from "./HashPwd.tsx";
 import DatePickerTest from "./DatePickerTest.tsx";
 import ScrollTest from "./ScrollTest.tsx";
 import Test from "./Test.tsx";
+import Bubbling from "./Bubbling.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Test />
+  <Bubbling />
 );
