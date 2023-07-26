5559c727b9d5c3c1950966a514b7dabab42e38aa
umsungjun <umseongjun@naver.com>
2023-07-26 10:51:48 +0900

    link 연습

--- react_practice/src/main.tsx
+++ react_practice/src/main.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
 import HashPwd from "./HashPwd.tsx";
-
+import { BrowserRouter } from "react-router-dom";
 import DatePickerTest from "./DatePickerTest.tsx";
 import ScrollTest from "./ScrollTest.tsx";
 import Test from "./Test.tsx";
 import Bubbling from "./Bubbling.tsx";
 import Center from "./Center.tsx";
+import LinkPractice from "./LinkPractice.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Center />
+  <BrowserRouter>
+    <LinkPractice />
+  </BrowserRouter>
 );
