1fa29df53fea4d4250422464bcda0a08002aea71
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-08-10 21:42:50 +0900

    접미사 배열

--- 2023.08/20230810.js
+++ 2023.08/20230810.js
@@ -58,3 +58,27 @@ function solution(a, b, c) {
   }
   return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
 }
+
+// 접미사 배열
+// 문제 설명
+// 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+// 문자열 my_string이 매개변수로 주어질 때, my_string의 모든 접미사를 사전순으로 정렬한 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// my_string은 알파벳 소문자로만 이루어져 있습니다.
+// 1 ≤ my_string의 길이 ≤ 100
+// 입출력 예
+// my_string	result
+// "banana"	["a", "ana", "anana", "banana", "na", "nana"]
+// "programmers"	["ammers", "ers", "grammers", "mers", "mmers", "ogrammers", "programmers", "rammers", "rogrammers", "rs", "s"]
+
+function solution(my_string) {
+  const answer = [];
+  const arr = my_string.split("");
+
+  for (let i = 0; i < my_string.length; i++) {
+    answer.push(arr.join(""));
+    arr.shift();
+  }
+  return answer.sort();
+}
