daa4463c13728679ab5ebceec3b9225790435cc1
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-08-19 23:26:49 +0900

    글자 지우기

--- 2023.08/20230819.js
+++ 2023.08/20230819.js
@@ -25,3 +25,27 @@ function solution(board, k) {
   }
   return answer;
 }
+
+// 글자 지우기
+// 문제 설명
+// 문자열 my_string과 정수 배열 indices가 주어질 때, my_string에서 indices의 원소에 해당하는 인덱스의 글자를 지우고 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ indices의 길이 < my_string의 길이 ≤ 100
+// my_string은 영소문자로만 이루어져 있습니다
+// 0 ≤ indices의 원소 < my_string의 길이
+// indices의 원소는 모두 서로 다릅니다.
+// 입출력 예
+// my_string	indices	result
+// "apporoograpemmemprs"	[1, 16, 6, 15, 0, 10, 11, 3]	"programmers
+
+function solution(my_string, indices) {
+  let answer = "";
+
+  my_string.split("").forEach((str, i) => {
+    if (!indices.includes(i)) {
+      answer += str;
+    }
+  });
+  return answer;
+}
