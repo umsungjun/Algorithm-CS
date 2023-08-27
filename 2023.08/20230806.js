40833ef931e8806f8d6d995da08836e1c11d3ace
umsungjun <umseongjun@naver.com>
2023-08-27 21:06:25 +0900

    Update 20230806.js

--- 2023.08/20230806.js
+++ 2023.08/20230806.js
@@ -61,7 +61,6 @@ function solution(arr) {
 function solution(myString) {
   return myString.split("x").map((str) => str.length);
 }
-
 // 문제 설명
 // 정수 배열 arr가 주어집니다. 이때 arr의 원소는 1 또는 0입니다. 정수 idx가 주어졌을 때, idx보다 크면서 배열의 값이 1인 가장 작은 인덱스를 찾아서 반환하는 solution 함수를 완성해 주세요.
 
