f9542b0db12858b827c92f1235e5f4a9bbf64a1d
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-15 23:15:55 +0900

    java 문제 풀이

---
+++ 2023.09/20230915.js
@@ -0,0 +1,23 @@
+/* class Main {
+
+    public static void main(String[] args) {
+
+        int x = 1, T_x=0, t_x=0;
+
+        T_x = (x >= 0) ? x : -x;
+
+        if(x >= 0)
+
+            t_x = x;
+
+        else
+
+            t_x = -x;
+
+        System.out.println(T_x+" "+t_x);
+
+    }
+
+} 
+
+1 1*/
