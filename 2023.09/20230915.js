00fd3c7917974a1c255a6cf13d59c022fbec68cd
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-15 23:27:01 +0900

    C언어 - 1부터 35까지 짝수 홀수 개수 구하기

--- 2023.09/20230915.js
+++ 2023.09/20230915.js
@@ -21,3 +21,26 @@
 } 
 
 1 1*/
+
+/* int main(void) {
+
+    int num=35, evencnt=0, oddcnt=0;
+
+    for(int i=1; i<=num; i++){
+
+        if(i%2==0)
+
+            evencnt++;
+
+        else
+
+            oddcnt++;
+
+    }
+
+    printf("%d %d", evencnt, oddcnt);
+
+
+} 
+17 18
+*/
