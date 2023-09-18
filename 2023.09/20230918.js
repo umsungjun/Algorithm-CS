bc386d71d660decfef999a41e13c544f4e4adf7b
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-18 21:58:21 +0900

    C언어

---
+++ 2023.09/20230918.js
@@ -0,0 +1,29 @@
+/* #include
+
+int main(void) {
+
+	int a = 27;
+
+	int b = 12;
+
+	int l, g;
+
+	for(int i=b; i>0; i--) {
+
+		if(a % i == 0 && b % i == 0) {
+
+			g = i;
+
+			break;
+
+		}
+
+	}
+
+	l = a * b / g;
+
+	printf("%d", g+l);
+
+}
+
+- 답안: 111 */
