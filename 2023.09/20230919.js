d694c4b1bd596fc7f6de8e12ad795e63445b298e
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-19 21:14:16 +0900

    데이터베이스 정의

--- 2023.09/20230919.js
+++ 2023.09/20230919.js
@@ -7,3 +7,25 @@
 ③ 물리
 
 - 답안: ① 외부 ② 개념 ③ 내부 */
+
+/* 데이터베이스 정의
+
+
+① Operational Data 
+
+② Shared Data 
+
+③ Stored Data 
+
+
+④ Integrated Data 
+
+ⓐ 중복이 최소화
+
+ⓑ 저장 매체에 저장
+
+ⓒ 반드시 필요한
+
+ⓓ 공동으로 사용
+
+- 답안: ⓐ ④ ⓑ ③ ⓒ ① ⓓ ② */
