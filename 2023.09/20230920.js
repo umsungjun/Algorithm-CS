69d514de154f6e0ec7fe378ffdfd9a1ab663fa54
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-20 21:18:54 +0900

    JAVA

--- 2023.09/20230920.js
+++ 2023.09/20230920.js
@@ -5,3 +5,28 @@
 /* () 테스트 : 모듈 테스트, 구현 단계, 블랙박스/화이트박스, 스텁, 드라이브
 
 - 답안: 단위(Unit) */
+
+/* class Gisafirst {
+
+	public static void main(String[] args) {
+
+		int i = 17;
+
+		i += 1;
+
+		i -= 2;
+
+		i *= 3;
+
+		i /= 4;
+
+		i %= 5;
+
+		System.out.println(i);
+
+	}
+
+
+} 
+
+- 답안: 2 */
