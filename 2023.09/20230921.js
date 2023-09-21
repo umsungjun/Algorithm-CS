37b71cd73b605fdf65441c6ba13a90bc22c09f32
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-21 23:22:11 +0900

    JAVA

---
+++ 2023.09/20230921.js
@@ -0,0 +1,28 @@
+/* class Gisafirst {
+
+	public static void main(String[] args) {
+
+		int a = 26;
+
+		int b = 91;
+
+		int g = 0;
+
+		int c = a < b? a : b;
+
+		for(int i = 1; i < c; i ++) {
+
+			if(a % i == 0 && b % i == 0) 
+
+				g = i;
+
+		}
+
+		System.out.println(g);
+
+	}
+
+
+} 
+
+- 답안: 13 */
