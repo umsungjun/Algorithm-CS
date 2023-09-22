63e8252f57453f25be70bbd0750eca79a8f0b2ec
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-22 22:58:03 +0900

    라우팅 프로토콜, 벨만 포드 알고리즘, 홉 = 거리 벡터 알고리즘

--- 2023.09/20230922.js
+++ 2023.09/20230922.js
@@ -1,3 +1,20 @@
 /* πFNAME, LNAME, SALARY(σDNO=1005)EMP 를 SQL로 작성
 
 - 답안: SELECT FNAME, LNAME, SALARY FROM EMP WHERE DNO=1005 */
+
+/* #include <stdio.h>
+
+int main(void) {
+    char a[3][5] = {"KOR", "HUS", "RES"};
+    char *pa[] = {a[0], a[1], a[2]};
+
+    int n = sizeof(pa) / sizeof(pa[0]); // 3 / 1
+
+    for (int i = 0; i < n; i++) {
+        printf("%c", pa[i][i]);
+    }
+} */
+
+/* 라우팅 프로토콜, 벨만 포드 알고리즘, 홉
+
+- 답안: 거리 벡터 알고리즘 */
