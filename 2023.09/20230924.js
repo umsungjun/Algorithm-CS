467676b9664a298ce72948effe73d5c944b64f35
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-09-24 19:54:54 +0900

    JAVA

---
+++ 2023.09/20230924.js
@@ -0,0 +1,30 @@
+/* class Gisafirst {
+
+	public static void main(String[] args) {
+
+		int a[] = new int[8];
+
+		int i = 0;
+
+		int n = 11;
+
+		while(n > 0) {
+
+			a[i++] = n % 2;
+
+			n /= 2;
+
+		}
+
+		for(i=7; i >= 0; i--) {
+
+			System.out.printf("%d", a[i]);
+
+		}
+
+	}
+
+
+}
+
+- 답안: 00001011 */
