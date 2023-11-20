1a14f02e4af3e57b2aa0aa6e1f5ae0df82b753da
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-11-20 18:38:44 +0900

    vite start

--- react_practice/src/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Test() {
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(typeof Number(e.target.value));
-  };
-
-  return <input onChange={(e) => handleInput(e)} />;
-}
