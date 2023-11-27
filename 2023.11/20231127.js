c7e828dde7afb9fb1af4b49c025e33ba4aa5bda3
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-11-27 20:54:27 +0900

    카펫 나만의 풀이로 복습

--- 2023.11/20231127.js
+++ 2023.11/20231127.js
@@ -1,20 +1,18 @@
 function solution(brown, yellow) {
-    const answer = [];
-    const hap = brown + yellow;
-    
-    for(let h=3; h<=hap; h++){
-        if(hap % h ===0){
-            let w = hap / h; 
-                             
-            if((w-2) * (h-2) === yellow){
-                return [w,h]
-            }
-            
-        }
+  const answer = [];
+  const hap = brown + yellow;
+
+  for (let h = 3; h <= hap; h++) {
+    if (hap % h === 0) {
+      let w = hap / h;
+
+      if ((w - 2) * (h - 2) === yellow) {
+        return [w, h];
+      }
     }
-    
-    
-    return answer;
+  }
+
+  return answer;
 }
 
 // 카펫 나만의 풀이로 복습
