1578255bc8fe4c4e28086f0f4a6d0e36ac040955
umsungjun <umseongjun@naver.com>
2023-12-07 13:22:19 +0900

    test

---
+++ 2023.12/20231204.js
@@ -0,0 +1,10 @@
+// const body = document.querySelector('body');
+
+// const weldingLabel = document.createElement('label');
+// welding.className = 'group1';
+
+// const replaced = document.createElement('label');
+// replaced.className = 'group1';
+
+// body.appendChild(welding);
+// body.appendChild(replaced);
