6f6e25501e948329e731983911f605bcfc7b282f
umsungjun <umseongjun@naver.com>
2024-01-04 16:44:58 +0900

    오토위니 스켈레톤 UI
    오토위니 스켈레톤 UI

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,13 +3,12 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const YourComponent = () => {
-  const [startDate, setStartDate] = useState(new Date());
-
   return (
     <>
       <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
+        selected={null}
+        showTimeSelect
+        onChange={(date) => console.log(date)}
       />
     </>
   );
