57df292fffa98a85027b8b9c46b8dfd1c101d7bc
umsungjun <umseongjun@naver.com>
2024-02-09 21:03:39 +0900

    [Leet Code] 2677. Chunk Array
    [Leet Code] 2677. Chunk Array

---
+++ 2024.02/2024.02.09.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+  const chunkArr = [];
+  let index = 0;
+  while (index < arr.length) {
+    chunkArr.push(arr.slice(index, index + size));
+    index += size;
+  }
+  return chunkArr;
+};
