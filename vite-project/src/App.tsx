955146f07049e12109126bd75adbe144e91f4bd9
umsungjun <umseongjun@naver.com>
2024-02-17 23:43:53 +0900

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,17 +3,7 @@ import ShowMoreText from "react-show-more-text";
 const YourComponent = () => {
   return (
     <>
-      <ShowMoreText
-        lines={3}
-        more="Read More"
-        less="Show less"
-        // className="content-css"
-        // anchorClass="show-more-less-clickable"
-        // onClick={this.executeOnClick}
-        // expanded={false}
-        width={280}
-        // truncatedEndingComponent={"... "}
-      >
+      <ShowMoreText lines={3} more='Read More' less='Show less' width={280}>
         We work with Experienced and certified dismantles in S.Korea. They will
         carefully dismantle your vehicles to your very order and satisfaction.
       </ShowMoreText>
