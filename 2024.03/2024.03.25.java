25dcfe1404ead7df2cd44b28c4f669b8a57808e6
umsungjun <umseongjun@naver.com>
2024-03-25 21:46:45 +0900

    정보처리 산업기사 java문제

--- 2024.03/2024.03.25.java
+++ 2024.03/2024.03.25.java
@@ -13,3 +13,31 @@ public class Test{
     }
 }
 // 정답 : 10
+
+class Berry{
+    String str;
+    void frt(){
+        str = "Berry"; // str변수 초기화
+        func(); // 오버라이딩에 의해 같은 이름일 때 자식의 메서드를 실행 // 3 호출
+    }
+    void func(){
+        System.out.println(str); // Berry 출력
+    }
+}
+class Apple extends Berry{
+    String str;
+    void func(){ // 2. 실행 
+        str = "Apple"; // str 변수 초기화
+        super.func(); // 부모의 func를 실행
+        System.out.prinln(str); // Apple 출력
+    }
+}
+public class Test{
+    public static void main(String[] args){
+        Berry A = new Apple();
+        A.frt() // 1. 호출
+    }
+}
+
+// 정답 : Berry
+     //  Apple
