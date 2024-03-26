6f64e3ffba0145f8af763d49906f4073829d3d47
umsungjun <umseongjun@naver.com>
2024-03-26 21:14:35 +0900

    정보처리 산업기사 python 문제

--- 2024.03/2024.03.26.py
+++ 2024.03/2024.03.26.py
@@ -7,3 +7,14 @@ print(asia)
 
 정답 : {'한국', '중국', '베트남', '홍콩', '태국'}
 파이썬에서 {}세트자료형은 중복 요소가 추가되지 않는다.
+
+
+class CharClass:
+    a = ['Seoul', 'Kyeongi', 'Inchon', 'Daejeon', 'Daegu', 'Pusan']
+myVar = CharClass() // myVar CharClass 저장
+str01 = ' ' // str01을 ' '으로 초기화
+for i in myVar.a: // a.length 만큼 반복 함
+    str01 = str + i[0] 
+print(str01) 
+
+정답 : SKIDDP
