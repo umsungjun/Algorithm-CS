85ccfcdfd67664ac3230cfdeeff1a675034dad24
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-03-30 16:31:24 +0900

    정보처리 산업기사 java문제

---
+++ 2024.03/2024.03.30.java
@@ -0,0 +1,18 @@
+public class Problem{
+    public static void main(String[] args){
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = prnt(a, b);
+        System.out.printf("a=%d, b=%d, c=%d\n", a, b, c);
+
+        static int prnt(x, y){
+            int z;
+            if(x === y) z = z + y;;
+            else z = x - y;
+            return(z);
+        }
+    }
+}
+
+정답: a=10, b=20, c=-10
