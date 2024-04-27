8cf26dc3c273892b7c6a934f56d848b20cbd56d8
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-04-27 23:53:07 +0900

    Q2. 암호문

---
+++ 2024.04/2024.04.27.js
@@ -0,0 +1,16 @@
+function solution(data) {
+  let count = 0;
+  const patternArr = ["r", "e", "v"];
+  data.split("").forEach((str, index) => {
+    const nextNum = +data[index + 1];
+    if (patternArr.includes(str) && !isNaN(nextNum)) {
+      if (nextNum === 1 && data[index + 2] == 0) {
+        count += 10;
+      } else {
+        count += nextNum;
+      }
+    }
+  });
+  const strCount = count + "";
+  return `${strCount[0]}월 ${strCount[1]}일`;
+}
