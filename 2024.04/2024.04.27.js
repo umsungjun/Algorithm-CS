f0823d3ddf030747b4d335b699bb976ba88173b8
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-05-03 23:48:12 +0900

    문자열에서 rev뒤에 나오는 숫자 출력 정규식

--- 2024.04/2024.04.27.js
+++ 2024.04/2024.04.27.js
@@ -16,3 +16,6 @@ function solution(data) {
   const strCount = count + "";
   return `${strCount[0]}월 ${strCount[1]}일`;
 }
+
+/* 문자열에서 rev뒤에 나오는 숫자 출력 정규식*/
+"a10b9r1ce33uab8wc918v2cv11v9".match(/([rev])(10|[0-9])/g);
