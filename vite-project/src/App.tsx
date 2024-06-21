f995a1a93e33ca7751599c144bbca805605d60a3
umsungjun <umseongjun@naver.com>
2024-06-21 10:34:38 +0900

    웹 접근성 테스트

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,18 +1,9 @@
 import { useState } from "react";
 
 const YourComponent = () => {
-  const [inputVal, setInputVal] = useState("");
-  console.log(inputVal);
-  return (
-    <>
-      <input type='text' onChange={(e) => setInputVal(e.target.value)} />
-      {(inputVal.includes("MAEU") || inputVal[0] === "2") && <div>MAERSK</div>}
-      {inputVal.includes("EUKO") && <div>EUKO</div>}
-      {inputVal.includes("SEL") && <div>CMA</div>}
-      {inputVal.includes("SEL") && <div>PIL</div>}
-      {inputVal.includes("SEL") && <div>HMM</div>}
-    </>
-  );
+  const [state, setState] = useState(true);
+
+  return <>{state ? "選択済み" : "無選択"}</>;
 };
 
 export default YourComponent;
