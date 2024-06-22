1342a8afd954fa212a49a3c80bf65cca352e6e2d
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-06-22 17:00:52 +0900

    Remove: vite project

--- vite-project/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const YourComponent = () => {
-  const [state, setState] = useState(true);
-
-  return <>{state ? "選択済み" : "無選択"}</>;
-};
-
-export default YourComponent;
