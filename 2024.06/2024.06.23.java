08363b6ba64f8faefe9224ee229c62a0b394a9a0
umsungjun <99730910+umsungjun@users.noreply.github.com>
2024-06-23 20:50:36 +0900

    정보처리 산업기사 java

---
+++ 2024.06/2024.06.23.java
@@ -0,0 +1,13 @@
+class Test{
+    public static void main(String args[]){
+        int i = 17;
+        i += 1; // 18
+        i -= 2; // 16
+        i *= 3; // 48
+        i /= 4; // 12
+        i %= 5; // 2
+        System.out.print(i);
+    }
+}
+
+// 정답 : 2
