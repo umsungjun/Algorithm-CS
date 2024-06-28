fa47fa4fc6c73ca3f558568bded52e4ffae01fec
umsungjun <umseongjun@naver.com>
2024-06-28 11:40:13 +0900

    정보처리 산업기사 c

---
+++ 2024.06/2024.06.28.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(){
+    // AscII_Code 'A' = 65, 'a' = 97
+    int a = 10;
+    char b = 'a';
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%c", b)
+}
+
+/* 정답: 10
+        97
+        a
+  */
