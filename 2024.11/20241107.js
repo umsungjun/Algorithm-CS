15d8f2897ad1f4e1188a6fdce3ccf93469bc0562
umsungjun <umseongjun@naver.com>
2024-11-07 08:31:13 +0900

    클로저 테스트

---
+++ 2024.11/20241107.js
@@ -0,0 +1,11 @@
+function outerFunction(outerVariable) {
+  return function innerFunction(innerVariable) {
+    console.log('Outer Variable: ' + outerVariable);
+    console.log('Inner Variable: ' + innerVariable);
+  };
+}
+
+const newFunction = outerFunction('outside');
+newFunction('test1');
+const newFunction2 = outerFunction('outside2');
+newFunction2('test2');
