319be713ed50b5c13d9a2dce1abf939aa35a7ef8
umsungjun <umseongjun@naver.com>
2025-11-09 20:07:02 +0900

    Update 2025.11.09.js

--- 2025.11/2025.11.09.js
+++ 2025.11/2025.11.09.js
@@ -13,3 +13,18 @@ function solution(n) {
 
   return x - 1;
 }
+
+// 프로그래머스 - 수박수박수박수박수박수?
+function solution(n) {
+  let answer = "";
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      answer += "박";
+    } else {
+      answer += "수";
+    }
+  }
+
+  return answer;
+}
