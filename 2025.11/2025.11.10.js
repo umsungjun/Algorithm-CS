1d752e4f99b6aedc11cc54a87e28874e84405a49
umsungjun <umseongjun@naver.com>
2025-11-10 21:32:42 +0900

    Update 2025.11.10.js

--- 2025.11/2025.11.10.js
+++ 2025.11/2025.11.10.js
@@ -38,3 +38,26 @@ function solution(arr) {
 
   return answer;
 }
+
+// 프로그래머스 - 약수의 개수와 덧셈
+function solution(left, right) {
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    const arr = [];
+
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        arr.push(j);
+      }
+    }
+
+    if (arr.length % 2 === 0) {
+      answer += i;
+    } else {
+      answer -= i;
+    }
+  }
+
+  return answer;
+}
