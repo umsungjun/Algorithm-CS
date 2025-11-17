cb932178552b76f5070ada51258cb197f1170c4f
umsungjun <umseongjun@naver.com>
2025-11-17 21:26:43 +0900

    프로그래머스 - 직사각형 별찍기 문제 풀이

--- 2025.11/2025.11.17.js
+++ 2025.11/2025.11.17.js
@@ -4,3 +4,23 @@ function solution(s) {
 
   return `${Math.min(...numSarr)} ${Math.max(...numSarr)}`;
 }
+
+// 프로그래머스 - 직사각형 별찍기
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  let answer = "";
+
+  for (let i = 0; i < b; i++) {
+    let line = "";
+    for (let j = 0; j < a; j++) {
+      line += "*";
+    }
+    line += "\n";
+    answer += line;
+  }
+
+  console.log(answer);
+});
