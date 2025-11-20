496683716ee913b87f5159649cbe256d19e647a7
umsungjun <umseongjun@naver.com>
2025-11-20 21:38:39 +0900

    프로그래머스 - 전국 대회 선발 고사 문제 풀이

--- 2025.11/2025.11.20.js
+++ 2025.11/2025.11.20.js
@@ -6,3 +6,21 @@ function solution(spell, dic) {
 
   return soltedDic.includes(soltedSpell) ? 1 : 2;
 }
+
+// 프로그래머스 - 전국 대회 선발 고사
+// 왜 이런 문제를 만들까?
+// 정렬 된 Rank의 index를 구하는 문제
+function solution(rank, attendance) {
+  const posibleRankList = [];
+
+  attendance.forEach((bool, i) => {
+    if (bool === true) {
+      posibleRankList.push(rank[i]);
+    }
+  });
+  posibleRankList.sort((a, b) => a - b);
+
+  const indexOfRank = posibleRankList.map((n) => rank.indexOf(n));
+
+  return 10000 * indexOfRank[0] + 100 * indexOfRank[1] + indexOfRank[2];
+}
