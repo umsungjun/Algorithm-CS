53868ad577ecea11d670e0bf99010ef0f6bc6567
umsungjun <umseongjun@naver.com>
2025-11-22 19:07:14 +0900

    프로그래머스 - 직사각형 넓이 구하기 문제 풀이

---
+++ 2025.11/2025.11.22.js
@@ -0,0 +1,29 @@
+// 프로그래머스 - 직사각형 넓이 구하기
+function solution(dots) {
+  let highX = -256;
+  let rowX = 256;
+  let highY = -256;
+  let rowY = 256;
+
+  dots.forEach(([x, y]) => {
+    if (x > highX) {
+      highX = x;
+    } else if (x < rowX) {
+      rowX = x;
+    }
+
+    if (y > highY) {
+      highY = y;
+    } else if (y < rowY) {
+      rowY = y;
+    }
+  });
+
+  const x = Math.abs(rowX - highX);
+  const y = Math.abs(rowY - highY);
+
+  return x * y;
+}
+
+// 직사각형의 너비 구하기
+// 가로 * 세로
