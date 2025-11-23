7f616a24e8062855b023f05f2e99f21fc5648e3f
umsungjun <umseongjun@naver.com>
2025-11-23 18:16:13 +0900

    프로그래머스 - 3총사 문제풀이

--- 2025.11/2025.11.23.js
+++ 2025.11/2025.11.23.js
@@ -6,3 +6,20 @@ function solution(n) {
 
 // 10진법 숫자를 3진법으로 변환 number.toString(3);
 // 3진법 숫자를 10진법으로 변환 parseInt(number, 3);
+
+// 프로그래머스 - 3총사
+function solution(number) {
+  let answer = 0;
+
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
