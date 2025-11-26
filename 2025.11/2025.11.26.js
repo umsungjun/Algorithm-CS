561b3bbe8d7133cc6e9e3f38b6ac85993bc8daa6
umsungjun <umseongjun@naver.com>
2025-11-26 19:50:06 +0900

    프로그래머스 - 다음 큰 숫자 문제 풀이

--- 2025.11/2025.11.26.js
+++ 2025.11/2025.11.26.js
@@ -12,3 +12,19 @@ function solution(n) {
 
   return arr[arr.length - 1];
 }
+
+// 프로그래머스 - 다음 큰 숫자
+const getHas1Cnt = (n2) => n2.split("").filter((num) => num == 1).length;
+
+function solution(n) {
+  const n2 = n.toString(2); // 2진수로 변환한 n
+  const n2oneCnt = getHas1Cnt(n2);
+
+  for (let i = n + 1; i <= 1_000_000; i++) {
+    const i2 = i.toString(2);
+    const i2oneCnt = getHas1Cnt(i2);
+    if (n2oneCnt === i2oneCnt) {
+      return i;
+    }
+  }
+}
