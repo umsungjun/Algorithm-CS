5594b1e579a1279a77a5e14060121d80a57cacbe
umsungjun <umseongjun@naver.com>
2025-12-01 21:04:45 +0900

    프로그래머스 - 짝지어 제거하기 문제풀이
    - 효율성 테스트 실패

---
+++ 2025.12/2025.12.01.js
@@ -0,0 +1,19 @@
+// 프로그래머스 - 짝지어 제거하기
+// 효율성 테스트를 통과 못함
+// 이유: 문자열의 길이가 1,000,000이 될 수 있기 때문에 모든 경우를 다 검사하면 시간 초과가 발생하는 것 같음
+function solution(s) {
+  if (s.length % 2 !== 0) return 0; // 짝수가 아니라면 0 return;
+
+  let splitS = s.split("");
+
+  for (let i = 0; i < splitS.length - 1; ) {
+    if (splitS[i] === splitS[i + 1]) {
+      splitS.splice(i, 2);
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return splitS.length === 0 ? 1 : 0;
+}
