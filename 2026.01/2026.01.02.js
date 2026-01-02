27b6aaab0925808c530406a234380a3cde22354f
umsungjun <umseongjun@naver.com>
2026-01-02 22:54:51 +0900

    더 효율적인 문제 풀이

--- 2026.01/2026.01.02.js
+++ 2026.01/2026.01.02.js
@@ -20,3 +20,13 @@ function solution(strings, n) {
 
   return strings;
 }
+
+// 더 간단하고 효율적인 문제 풀이
+// localeCompare 메서드는 두 문자열을 비교하여 정렬 순서를 결정하는 데 사용 a보다 b가 뒤에 오면 양수, 같으면 0, 앞에 오면 음수를 반환
+function solution(strings, n) {
+  strings.sort((a, b) => {
+    return a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]); // 같으면 전체 문자열 비교, 다르면 n번째 문자 비교
+  });
+
+  return strings;
+}
