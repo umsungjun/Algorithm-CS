3dfea33713f76e9353958feb81df6821e2ce9af7
umsungjun <umseongjun@naver.com>
2026-01-05 21:48:04 +0900

    프로그래머스 - 구명보트 문제풀이 1

---
+++ 2026.01/2026.01.05.js
@@ -0,0 +1,19 @@
+// 프로그래머스 - 구명보트
+// 정확성 테스트 통과 못함
+function solution(people, limit) {
+  people.sort((a, b) => a - b); // 무게 오름차순 정렬
+
+  let answer = 0;
+  let hap = 0;
+
+  for (let i = 0; i < people.length; i++) {
+    if (people[i] + people[i + 1] <= limit) {
+      answer++;
+      i++;
+    } else {
+      answer++;
+    }
+  }
+
+  return answer;
+}
