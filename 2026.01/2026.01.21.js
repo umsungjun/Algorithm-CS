bb5291d3339c2d9d9ded89f5ae5c0bfc55b2c228
umsungjun <umseongjun@naver.com>
2026-01-21 20:28:15 +0900

    프로그래머스 - 문자열 바꿔서 찾기 문제 풀이

--- 2026.01/2026.01.21.js
+++ 2026.01/2026.01.21.js
@@ -16,3 +16,14 @@ function solution(quiz) {
 
 // 실 운영 코드에서는 eval()을 보안 위협때문에 사용하지 않아야 함
 // eval()의 계산 값의 type은 number를 반환 하기 때문에 splitQ[1]을 Number로 형변환 함
+
+// 프로그래머스 - 문자열 바꿔서 찾기
+function solution(myString, pat) {
+  let str = "";
+
+  for (let s of myString) {
+    str += s === "A" ? "B" : "A";
+  }
+
+  return str.includes(pat) ? 1 : 0;
+}
