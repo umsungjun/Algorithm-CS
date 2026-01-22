401d361e93210ad84833310371ce817d3748804a
umsungjun <umseongjun@naver.com>
2026-01-22 21:03:50 +0900

    프로그래머스 - 외계행성의 나이 문제 풀이

---
+++ 2026.01/2026.01.22.js
@@ -0,0 +1,21 @@
+// 프로그래머스 - 외계행성의 나이
+function solution(age) {
+  const obj = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+    8: "i",
+    9: "j",
+  };
+
+  return age
+    .toString()
+    .split("")
+    .map((key) => obj[key])
+    .join("");
+}
