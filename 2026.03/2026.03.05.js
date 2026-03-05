1edfd13bfc990c2004a8ac4f04efc1c68190d209
umsungjun <umseongjun@naver.com>
2026-03-05 21:02:36 +0900

    프로그래머스 - 잘라서 배열로 저장하기 문제풀이

--- 2026.03/2026.03.05.js
+++ 2026.03/2026.03.05.js
@@ -2,3 +2,18 @@
 function solution(seoul) {
   return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
+
+// 잘라서 배열로 저장하기
+function solution(my_str, n) {
+  const answer = [];
+
+  for (let i = 0; i < my_str.length; i += n) {
+    if (i + n > my_str.length) {
+      answer.push(my_str.slice(i, my_str.length + 1));
+    } else {
+      answer.push(my_str.slice(i, i + n));
+    }
+  }
+
+  return answer;
+}
