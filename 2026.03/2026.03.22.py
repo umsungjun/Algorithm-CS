81cb82008825d30b9f1902cc93a4992a54bee24c
umsungjun <umseongjun@naver.com>
2026-03-22 21:44:15 +0900

    Create 2026.03.22.py

---
+++ 2026.03/2026.03.22.py
@@ -0,0 +1,7 @@
+a = ['Seoul', 'Kyunggi', 'Incheon', 'Daejeon', 'Daegu', 'Pusan']
+
+str01 = 'S'
+
+for i in a:
+    str01 = str01 + i[0] // S에 a배열의 1번째 글자들을 하나씩 붙여서 str01에 저장한다.
+print(str01) // Seynaau
