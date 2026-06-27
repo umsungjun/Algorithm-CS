51fe71db0b267e629aa3d7961b6f5f8754a14313
umsungjun <umseongjun@naver.com>
2026-06-27 23:08:17 +0900

    python 문제 풀이

--- 2026.06/2025.06.27.py
+++ 2026.06/2025.06.27.py
@@ -7,6 +7,19 @@ print({i: i ** 2 for i in t if i % 2 == 1})
 for i in t: // 1, 2, 3, 4, 5 순회
     if i % 2 == 1: // 홀수일 경우만 if문 통과
         print(f"{i}: {i ** 2}") 
-        
+
         답: {1: 1, 3: 9, 5: 25}
 """
+
+"""---"""
+
+lst = 'Hi Soojebi'
+print(lst[:-5:-1])
+
+""" 
+    역방향 정렬하면 ibejooS iH 순으로 정렬 됨
+    -1에서 시작해서 -5까지 슬라이싱하면 ibej만 출력됨
+
+    답: ibej
+"""
+
