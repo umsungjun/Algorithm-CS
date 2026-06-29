1d311d823c598f6f7c62073ac061ca1ab92ae530
umsungjun <umseongjun@naver.com>
2026-06-29 21:50:40 +0900

    Update 2026.06.29.java

--- 2026.06/2026.06.29.java
+++ 2026.06/2026.06.29.java
@@ -1,18 +1,29 @@
-class Gamja {
-    public static String fn(String str, int index, boolean[] seen) {
-        if (index < 0) return "";
-        char c = str.charAt(index); // index 위치의 문자를 가져옴
-        String result = fn(str, index - 1, seen);
-        if (!seen[c]) { // 해당 문자가 없을 때
-            seen[c] = true;
-            return c + result; // d + c + b + a
-        }
-        return result;
-    }
+public class Gamja {
     public static void main(String[] args) {
-        String str = "abacabcd"; // 문자형 str 변수에 문자열 "abacabcd"를 저장
-        int length = str.length(); // 8
-        boolean[] seen = new boolean[256]; // seen 배열을 256 크기로 초기화하여 모든 문자가 처음에는 등장하지 않았음을 나타냄
-        System.out.print(fn(str, length - 1, seen));
+        Parent c = new Child();
+        c.paint();
+        c.draw(); // 8. D출력
     }
-} 
+}
+class Parent {
+    public void paint() {
+        System.out.print("A");
+        draw();
+    }
+    public void draw() {
+        System.out.print("B"); // 2. B출력
+        draw(); // 3. 재귀 호출
+    }
+}
+class Child extends Parent {
+    public void paint() {
+        super.draw(); // 1. super.draw() 호출
+        System.out.print("C"); // 5. C출력
+        this.draw(); // 6. this.draw() 호출
+    }
+    public void draw() {
+        System.out.print("D"); // 4. D출력 7. D출력
+    }
+}
+
+// 정답: BDCDD  
