0c47bab1fe921038d7778e8f4998a606f6d61755
umsungjun <umseongjun@naver.com>
2026-06-29 21:29:26 +0900

    Update 2026.06.29.c

--- 2026.06/2026.06.29.c
+++ 2026.06/2026.06.29.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-char n[30];
-char *gamja(){
-    gets(n); // gets() 함수를 사용하여 문자열을 입력받아 n 배열에 저장
-    return n;
+#define SIZE 3
+
+typedef struct {
+    int a[SIZE]; // 배열 3의 크기
+    int front; // 0으로 초기화
+    int rear; // 0으로 초기화
+} Queue;
+
+void enq(Queue* q, int val){
+    q->a[q->rear] = val;
+    q->rear = (q->rear + 1) % SIZE;
 }
+
+int deq(Queue* q) {
+    int val = q->a[q->front];
+    q->front = (q->front + 1) % SIZE;
+    return val;
+}
+
 int main() {
-    char *p1=gamja(); // 배열 n의 주소를 p1에 저장
-    char *p2=gamja(); // 배열 n의 주소를 p2에 저장
-    char *p3=gamja(); // 배열 n의 주소를 p3에 저장
-    printf("%s\n", p1); // 박영희
-    printf("%s\n", p2); // 박영희
-    printf("%s\n", p3); // 박영희
+    Queue q = {{0}, 0, 0};
+
+    enq(&q,1); enq(&q,2); deq(&q); enq(&q, 3);
+
+    int first = deq(&q);
+    int second = deq(&q);
+    printf("%d 그리고 %d", first, second);
+
     return 0;
 }
-/* 
-[입력]
-홍길동
-김철수
-박영희 
-*/
+
+// 정답: 2 그리고 3
