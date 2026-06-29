bd78328978c9f00611177154942ffe7800782c2c
umsungjun <umseongjun@naver.com>
2026-06-29 21:41:06 +0900

    Update 2026.06.29.java

--- 2026.06/2026.06.29.java
+++ 2026.06/2026.06.29.java
@@ -1,24 +1,18 @@
-class A {
-    int a, b;
-    public A(int a, int b) {
-        this.a = a;
-        this.b = b;
+class Gamja {
+    public static String fn(String str, int index, boolean[] seen) {
+        if (index < 0) return "";
+        char c = str.charAt(index); // index 위치의 문자를 가져옴
+        String result = fn(str, index - 1, seen);
+        if (!seen[c]) { // 해당 문자가 없을 때
+            seen[c] = true;
+            return c + result; // d + c + b + a
+        }
+        return result;
     }
-}
-
-class B extends A {
-    int c = 3;
-    public B(int i) {
-        super(i, i + 1); // 부모 A의 생성자를 호출하여 a와 b 필드 초기화 a = 10, b = 11
-    }
-    public void print() {
-        System.out.println(c * c); // 9
-    }
-}
-
-public class Gamja {
     public static void main(String[] args) {
-        B a = new B(10); // B타입의 객체를 생성하여 a에 할당
-        a.print();
+        String str = "abacabcd"; // 문자형 str 변수에 문자열 "abacabcd"를 저장
+        int length = str.length(); // 8
+        boolean[] seen = new boolean[256]; // seen 배열을 256 크기로 초기화하여 모든 문자가 처음에는 등장하지 않았음을 나타냄
+        System.out.print(fn(str, length - 1, seen));
     }
-}
+} 
