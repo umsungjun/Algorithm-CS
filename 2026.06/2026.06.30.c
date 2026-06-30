b734691138f963f6e6557df17d46fc808fd8ceb2
umsungjun <umseongjun@naver.com>
2026-06-30 21:08:11 +0900

    Update 2026.06.30.c

--- 2026.06/2026.06.30.c
+++ 2026.06/2026.06.30.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+int Gamja(int base, int exp){
+    int i, result = 1;
+    for(i = 0; i < exp; i++) // 0~9 까지 실행
+        result *= base;
+    return result;
+}
 int main() {
-    int arr[3][3]={1, 2, 3, 4, 5, 6, 7, 8, 9};
-    /* 
-        1 2 3
-        4 5 6
-        7 8 9
-    */
-    int* parr[2]={arr[1], arr[2]}; // arr[1] = {4, 5, 6}, arr[2] = {7, 8, 9}
-    printf("%d", parr[1][1]+*(parr[1]+2)+**parr); // parr[1][1] = 8, *(parr[1]+2) = 9, **parr = 4
+    printf("%d", Gamja(2, 10));
     return 0;
 }
+
+/* 정답: 1024 */
