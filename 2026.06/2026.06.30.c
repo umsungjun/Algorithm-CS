59b194303da0539fa6e899c224308db8713cc79a
umsungjun <umseongjun@naver.com>
2026-06-30 20:42:36 +0900

    Update 2026.06.30.c

--- 2026.06/2026.06.30.c
+++ 2026.06/2026.06.30.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 3; i++) { // 1, 2, 3 실행
-        sum += i;
-    }
-    printf("%d", sum);
+    int arr[3][3]={1, 2, 3, 4, 5, 6, 7, 8, 9};
+    /* 
+        1 2 3
+        4 5 6
+        7 8 9
+    */
+    int* parr[2]={arr[1], arr[2]}; // arr[1] = {4, 5, 6}, arr[2] = {7, 8, 9}
+    printf("%d", parr[1][1]+*(parr[1]+2)+**parr); // parr[1][1] = 8, *(parr[1]+2) = 9, **parr = 4
     return 0;
 }
-
-/* 정답: 6 */
