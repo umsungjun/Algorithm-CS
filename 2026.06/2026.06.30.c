7611f9b4df426f2421e4cb24163af82057cc414e
umsungjun <umseongjun@naver.com>
2026-06-30 21:35:21 +0900

    Update 2026.06.30.c

--- 2026.06/2026.06.30.c
+++ 2026.06/2026.06.30.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-int Gamja(int base, int exp){
-    int i, result = 1;
-    for(i = 0; i < exp; i++) // 0~9 까지 실행
-        result *= base;
-    return result;
-}
+struct Gamja {
+    char name[10];
+    int age;
+};
 int main() {
-    printf("%d", Gamja(2, 10));
+    struct Gamja s[] = {"Kim", 28, "Lee", 38, "Seo", 50, "Park", 35}; // 배열 s의 각 요소는 struct Gamja 타입의 구조체로 구성되어 있음
+    struct Gamja *p; // 포인터 p를 선언하고, 구조체 배열 s의 첫 번째 요소를 가리키도록 함
+    p = s; // 포인터 p가 구조체 배열 s의 첫 번째 요소를 가리키도록 함
+    p++; // 포인터 p를 다음 구조체 요소로 이동시킴, 즉 s[1]을 가리키게 됨
+    printf("%s\n", p->name); // Lee
+    printf("%d\n", p->age); // 38
     return 0;
 }
-
-/* 정답: 1024 */
