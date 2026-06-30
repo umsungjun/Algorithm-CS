1b667a2a0a0a70dde386c63fa5315ed217a75f0a
umsungjun <umseongjun@naver.com>
2026-06-30 21:47:07 +0900

    Update 2026.06.30.java

--- 2026.06/2026.06.30.java
+++ 2026.06/2026.06.30.java
@@ -1,21 +1,12 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Integer a = 100;
-        Integer b = 100;
-        Integer c = 200;
-        Integer d = 200;
-        System.out.println(a == b);
-        System.out.println(c == d);
+        String str = "ITISTESTSTRING";
+        String[] result = str.split("T");
+        System.out.print(result[3]);
     }
 }
 
-
 /* 
- - Integer는 Int와 다른 객체로 감싼 래퍼 클래스
- - 값을 비교하려면 equals() 메서드를 사용해야 함
- - -128 ~ 127 범위의 Integer 객체를 JVM 내부에 미리 만들어 캐시
- - 그래서 a == b는 true, c == d는 false
-
- - 정답: true
-        false
+ - ['I', 'IS', 'ES', 'S', 'RING']
+ 정답: S
  */
