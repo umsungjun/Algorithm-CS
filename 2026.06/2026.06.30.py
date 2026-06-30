7b7eff922f4c563fc4e0e7cb54da9d2a186761d0
umsungjun <umseongjun@naver.com>
2026-06-30 21:32:02 +0900

    Update 2026.06.30.py

--- 2026.06/2026.06.30.py
+++ 2026.06/2026.06.30.py
@@ -1,7 +1,8 @@
-x = 10 # 전역 스코프 10
+scores = {"a": 2, "b": 4}
+scores["a"] += 3
+print(scores["a"] + scores.get("c", 1))
 
-def f():
-    x = 3 # 지역 스코프 3
-    return x
-
-print(f(), x) # 3 10 파이썬에서는 자동 ' '공백을 넣어줌
+""" 
+ - 파이썬에서 get() 메서드는 (key, default)로 동작
+ 정답: 6
+"""
