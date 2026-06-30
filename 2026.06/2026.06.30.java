435631e07021324f43179c9669e07a2f38d07047
umsungjun <umseongjun@naver.com>
2026-06-30 21:01:53 +0900

    Update 2026.06.30.java

--- 2026.06/2026.06.30.java
+++ 2026.06/2026.06.30.java
@@ -1,18 +1,21 @@
-class Person {
-     private String name;
-     public Person(String val) {
-         name = val;
-     }
-     public static String get() {
-         return name; // static 메서드에서는 인스턴스 변수에 접근할 수 없음
-     }
-     public void print() {
-         System.out.println(name);
-     }
+interface Person {
+    void sayHello(); // 추상 메서드 -> 구현 클래스가 반드시 오버라이딩 되야 함
+
+    default void sayBye() { // default 메서드는 구현 클래스에서 오버라이딩 하지 않아도 사용 가능
+        System.out.println("안녕히 가세요");
+    }
 }
-class Gamja {
+
+class Student implements Person {
+    public void sayHello() {
+        System.out.println("안녕하세요");
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        Person p = new Person("gamja"); // Person 타입 변수에 생성자로 인스턴스 생성
-        p.print();
+        Person p = new Student();
+        p.sayHello(); // '안녕하세요' 출력
+        p.sayBye(); // '안녕히 가세요' 출력
     }
 }
