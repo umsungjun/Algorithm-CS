22a228d0a95ddaa73c2b3866ddca9a59fe96702b
umsungjun <umseongjun@naver.com>
2026-07-01 22:09:51 +0900

    Update 2026.07.01.py

--- 2026.07/2026.07.01.py
+++ 2026.07/2026.07.01.py
@@ -1,9 +1,6 @@
-def f(a):
-    m = [[x] for x in a] # [[1], [2], [3], [4]]
-    b = m[:] # b = [[1], [2], [3], [4]]
-    for i in range(len(b) - 1): # 0~2까지 반복문 진행
-        b[i+1] += b[i] # [[1], [2, 1], [3, 2, 1], [4, 3, 2, 1]] 배열 끼리의 합집합
-    return sum(len(x) for x in m) # 정답: 10
-
-print(f([1, 2, 3, 4]))
-
+a = "engineer information processing"
+b = a[:3] # eng
+c = a[4:6] # ne
+d = a[29:] # ng
+e = b + c + d #engneng
+print(e)
