9f3176894d8a704797e0c5b1425c30a8f953da4b
umsungjun <umseongjun@naver.com>
2026-07-02 22:03:13 +0900

    Update 2026.07.02.c

--- 2026.07/2026.07.02.c
+++ 2026.07/2026.07.02.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-int fn(int a) {
-    if (a <= 1) return 1;
-    return a * fn(a - 1); // 1*2*3*4*5 = 120
+int increase(){
+    static int x = 0;
+    x+=2;
+    return x;
 }
+
 int main() {
-    int a;
-    scanf("%d", &a); // 5
-    printf("%d", fn(a));
+    int x = 0; // 4
+    int sum=0; // 20
+    int i = 0;
+    for(i=0;i<4;i++){ // 0~3 까지 반복
+        x++;
+        sum+=increase(); // 2, 4, 6, 8
+    }
+
+    printf("%d", sum); // 정답: 20
     return 0;
 }
