255629dd458170c330a8e34f1102f9d8eaf58b13
umsungjun <umseongjun@naver.com>
2026-07-02 21:37:39 +0900

    Update 2026.07.02.c

--- 2026.07/2026.07.02.c
+++ 2026.07/2026.07.02.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-void swap(int a, int b) {
-    int t=a; // t = 11;
-    a=b; // a = 19;
-    b=t; // b = 11;
-}
+#include <stdlib.h>
+
+struct Item {
+    int code;
+    char tag;
+};
+
 int main() {
-    int a=11;
-    int b=19;
-    swap(a, b); // swap 함수 호출 후에도 main 함수 내의 a, b 값은 변하지 않음
-    switch(a) {
-        case 19:
-            b+=1; 
-        case 11:
-            b+=2; // b = 19 + 2 = 21 switch문에서 break가 없으므로 case 11이후 모든 case문이 실행됨
-        default:
-            b+=3; // b = 21 + 3 = 24
-    }
-    printf("%d", a-b); // 정답: 13
+    struct Item *p = (struct Item *)malloc(sizeof(struct Item)); // 동적 메모리 할당
+    p->code = 7;
+    p->tag = 'B';
+    printf("%c%d", p->tag, p->code); // 정답: B7
+    free(p); // 동적 메모리 해제
     return 0;
 }
