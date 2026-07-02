c699580dd3005babbb3926b8337c9155e7fa0a01
umsungjun <umseongjun@naver.com>
2026-07-02 21:40:44 +0900

    Update 2026.07.02.java

--- 2026.07/2026.07.02.java
+++ 2026.07/2026.07.02.java
@@ -1,32 +1,20 @@
-class Animal {
-    String name;
-    Animal(String name) { this.name = name; }
-}
+class Counter {
+    static int n = 0; // static 변수는 모든 인스턴스가 같은 변수를 공유합니다. 
 
-class Dog extends Animal {
-    Dog(String name) { super(name); }
-    String bark() { return name + " 멍멍"; }
-}
-
-class Cat extends Animal {
-    Cat(String name) { super(name); }
-    String meow() { return name + " 야옹"; }
+    void add() {
+        n++;
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Animal a = new Dog("초코"); // 업캐스팅: 자식 객체를 부모 타입으로 참조
+        Counter a = new Counter();
+        Counter b = new Counter();
 
-        if (a instanceof Cat) { // Dog는 Cat의 인스턴스가 아니므로 false
-            Cat c = (Cat) a;
-            System.out.println(c.meow());
-        } else if (a instanceof Dog) { // a는 Dog의 인스턴스이므로 true
-            Dog d = (Dog) a; // 다운캐스팅: 부모 객체를 자식 타입으로 참조
-            System.out.println(d.bark());
-        }
+        a.add();
+        b.add();
+        a.add();
+
+        System.out.print(Counter.n); // 정답: 3
     }
 }
-
-/* 
- - 정답: 초코 멍멍
- */
