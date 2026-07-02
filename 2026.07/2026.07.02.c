02b09e9eb44b0fcc511ce1037bfc2bd7d4918e5e
umsungjun <umseongjun@naver.com>
2026-07-02 21:48:28 +0900

    Update 2026.07.02.c

--- 2026.07/2026.07.02.c
+++ 2026.07/2026.07.02.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct Item {
-    int code;
-    char tag;
-};
-
+int isPrime(int number) {
+    int i;
+    for (i = 2; i < number; i++) {
+        if (number % i == 0) return 0;
+    }
+    return 1;
+}
 int main() {
-    struct Item *p = (struct Item *)malloc(sizeof(struct Item)); // 동적 메모리 할당
-    p->code = 7;
-    p->tag = 'B';
-    printf("%c%d", p->tag, p->code); // 정답: B7
-    free(p); // 동적 메모리 해제
+    int number = 13195, max_div = 0, i;
+    for (i = 2; i < number; i++) // 2부터 13194까지 실행
+        if (isPrime(i) == 1 && number % i == 0)
+            max_div = i; // i가 소수이면서 13195의 약수이면 max_div에 저장
+    printf("%d", max_div); // 5, 7, 13, 29 중 가장 큰 소수 약수인 29 출력
     return 0;
 }
