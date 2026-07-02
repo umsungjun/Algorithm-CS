a5da741eb034e0821c6ded1a2b2c2ba8b657f303
umsungjun <umseongjun@naver.com>
2026-07-02 21:57:35 +0900

    Update 2026.07.02.c

--- 2026.07/2026.07.02.c
+++ 2026.07/2026.07.02.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-int isPrime(int number) {
-    int i;
-    for (i = 2; i < number; i++) {
-        if (number % i == 0) return 0;
-    }
-    return 1;
+int fn(int a) {
+    if (a <= 1) return 1;
+    return a * fn(a - 1); // 1*2*3*4*5 = 120
 }
 int main() {
-    int number = 13195, max_div = 0, i;
-    for (i = 2; i < number; i++) // 2부터 13194까지 실행
-        if (isPrime(i) == 1 && number % i == 0)
-            max_div = i; // i가 소수이면서 13195의 약수이면 max_div에 저장
-    printf("%d", max_div); // 5, 7, 13, 29 중 가장 큰 소수 약수인 29 출력
+    int a;
+    scanf("%d", &a); // 5
+    printf("%d", fn(a));
     return 0;
 }
